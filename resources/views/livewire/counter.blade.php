0528bd74cac8f6335bcab7146d6572c04a8039bb
masudrana03 <920mash@gmail.com>
2024-12-29 09:49:36 +0600

    wire-embed test succes.

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -10,14 +10,14 @@
 
 
     <div class="container">
-       <h1>Masud Tumi valo hoya jaw.</h1>
+       <h1>Shopify Announce It Embed Successfully...</h1>
     </div>
 
     <footer>
         <p>&copy; 2024 My Simple Page. All rights reserved.</p>
     </footer>
 
-  
+
 
 </div>
 
